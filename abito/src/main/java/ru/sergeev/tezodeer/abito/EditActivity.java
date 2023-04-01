2e38d2f0ebb12c9d98586050cbdbb93b4ed6f652
TEZOdeer <124118299+TEZOdeer@users.noreply.github.com>
2023-04-01 22:05:23 +0900

    01.04.2022 21:59 Abito

---
+++ abito/src/main/java/ru/sergeev/tezodeer/abito/EditActivity.java
@@ -0,0 +1,90 @@
+package ru.sergeev.tezodeer.abito;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
+public class EditActivity extends AppCompatActivity {
+    private ImageView imItem;
+    private StorageReference mStorageRef;
+    private Uri uploadUri;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_layout);
+        init();
+    }
+    private void init()
+    {
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+        imItem = (ImageView) findViewById(R.id.imItem);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 10 && data != null && data.getData() != null)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                imItem.setImageURI(data.getData());
+                uploadImage();
+            }
+        }
+    }
+    private void uploadImage()
+    {
+        Bitmap bitmap = ((BitmapDrawable)imItem.getDrawable()).getBitmap();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100,out);
+        byte[] byteArray = out.toByteArray();
+        final StorageReference mRef = mStorageRef.child(System.currentTimeMillis() + "_image");
+        UploadTask up = mRef.putBytes(byteArray);
+        Task<Uri> task = up.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return mRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+            uploadUri = task.getResult();
+                Toast.makeText(EditActivity.this, "Upload done: " + uploadUri.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    public void onClickImage (View v) {
+        getImage();
+    }
+    private void getImage()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 10);
+    }
+}
--- abito/src/main/java/ru/sergeev/tezodeer/abito/MainActivity.java
+++ abito/src/main/java/ru/sergeev/tezodeer/abito/MainActivity.java
@@ -1,16 +1,39 @@
 package ru.sergeev.tezodeer.abito;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView nav_view;
+    private DrawerLayout drawerLayout;
+    private FirebaseAuth mAuth;
+    private TextView userEmail;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +41,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         init();
     }
-
     private void init()
     {
         nav_view = findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         nav_view.setNavigationItemSelectedListener(this);
+        userEmail = (TextView) nav_view.getHeaderView(0).findViewById(R.id.tvEmail);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("abito");
+        myRef.setValue("Hello, World!");
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    private void getUserData()
+    {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null)
+        {
+            userEmail.setText(currentUser.getEmail());
+        }
+        else
+        {
+            userEmail.setText(R.string.sign_in_or_sign_up);
+        }
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -46,15 +92,101 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(this, "Pressed id My Dm", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_up:
+                signUpDialog(R.string.sign_up,R.string.sign_up_button, 0);
                 Toast.makeText(this, "Pressed id Sign Up", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_in:
+                signUpDialog(R.string.sign_in,R.string.sign_in_button, 1);
                 Toast.makeText(this, "Pressed id Sign In", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_out:
+                signOut();
                 Toast.makeText(this, "Pressed id Sign Out", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.startz:
+                Intent a = new Intent(this, EditActivity.class);
+                startActivity(a);
+
         }
         return false;
+    }
+    private void signUpDialog(int title, int buttonTitle,int index)
+    {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.sign__up_layout, null);
+        dialogBuilder.setView(dialogView);
+        TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+        titleTextView.setText(title);
+        EditText edEmail = dialogView.findViewById(R.id.edEmail);
+        EditText edPassword = dialogView.findViewById(R.id.edPassword);
+        Button b = dialogView.findViewById(R.id.buttonSignUp);
+        b.setText(buttonTitle);
+        b.setOnClickListener((v -> {
+                if(index==0)
+                {
+                    signUp(edEmail.getText().toString(), edPassword.getText().toString());
+                }
+                else
+                {
+                    signIn(edEmail.getText().toString(), edPassword.getText().toString());
+                }
+                dialog.dismiss();
+            }
+        ));
+        dialog = dialogBuilder.create();
+        dialog.show();
+    }
+    private void signUp(String email, String password)
+    {
+        if(!email.equals("") && !password.equals(""))
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            getUserData();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+     else
+        {
+            Toast.makeText(this, "Email или Password пустой!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void signIn(String email, String password)
+    {
+        if(!email.equals("") && !password.equals(""))
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            getUserData();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        else
+        {
+            Toast.makeText(this, "Email или Password пустой!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void signOut()
+    {
+        mAuth.signOut();
+        getUserData();
     }
 }
