bd7febbbccc31d3aab39cf5a9388956331063c68
tezodeer <tezodeer@gmail.com>
2023-04-27 20:21:55 +0900

    27.04.2023 Yaksecast beta

--- yaksecast/src/main/java/ru/sergeev/yaksecast/MainActivity.java
+++ yaksecast/src/main/java/ru/sergeev/yaksecast/MainActivity.java
@@ -1,35 +1,25 @@
 package ru.sergeev.yaksecast;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.provider.ContactsContract;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.time.Instant;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.Calendar;
-import java.util.Date;
 
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -43,29 +33,41 @@ public class MainActivity extends AppCompatActivity {
     TextView tvTemp, tvDesk, tvTime;
     ImageView ivIcon;
     int dateTime;
+    TextView tvForecast1,tvForecast2,tvForecast3,tvForecast4,tvForecast5,tvForecast6,tvForecast7,tvForecast8,tvSunrise,tvSunset;
+    ImageView imForecast1,imForecast2,imForecast3,imForecast4,imForecast5,imForecast6,imForecast7,imForecast8;
+    TextView tvForecastTemp1,tvForecastTemp2,tvForecastTemp3,tvForecastTemp4,tvForecastTemp5,tvForecastTemp6,tvForecastTemp7,tvForecastTemp8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Date time = Calendar.getInstance().getTime();
         init();
     }
 
     private void init() {
+        tvSunrise = findViewById(R.id.tvSunrise);
+        tvSunset = findViewById(R.id.tvSunset);
         tvTemp = findViewById(R.id.tvTemp);
         ivIcon = findViewById(R.id.ivIcon);
         tvDesk = findViewById(R.id.tvDesk);
         tvTime = findViewById(R.id.tvTime);
-
         api_key();
     }
 
-    private void api_key() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        api_key_main();
+//        api_key();
+    }
+
+    private void api_key_main() {
         Logger.getLogger(OkHttpClient.class.getName()).setLevel(Level.FINE);
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("https://api.openweathermap.org/data/2.5/weather?q=yakutsk&appid=23df53519973b7a0f5b39b79e5b9aec4&units=metric&lang=ru")
+//                .url("https://api.openweathermap.org/data/2.5/forecast?q=Yakutsk&appid=23df53519973b7a0f5b39b79e5b9aec4&units=metric&lang=ru")
                 .get()
                 .build();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -77,44 +79,156 @@ public class MainActivity extends AppCompatActivity {
                 public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 }
 
+                @SuppressLint("SimpleDateFormat")
                 @Override
                 public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                     String responseData = response.body().string();
                     Logger.getLogger(OkHttpClient.class.getName()).setLevel(Level.FINE);
                     try {
                         JSONObject json = new JSONObject(responseData);
+                        JSONObject main = json.getJSONObject("main");
+                        String temp = main.getString("temp");
 
-                        JSONObject temp1 = json.getJSONObject("main");
+                        JSONObject sys = json.getJSONObject("sys");
+                        int sunrise = sys.getInt("sunrise");
+                        String sunrise_text = new java.text.SimpleDateFormat("HH:mm").format(new Date(sunrise * 1000L));
 
+                        int sunset = sys.getInt("sunset");
+                        String sunset_text = new java.text.SimpleDateFormat("HH:mm").format(new Date(sunset * 1000L));
 
-                        JSONArray weather = json.getJSONArray("weather");
-                        JSONObject object = weather.getJSONObject(0);
+                        JSONArray main_weather = json.getJSONArray("weather");
+                        JSONObject main_weather_icon = main_weather.getJSONObject(0);
+                        String main_icon = main_weather_icon.getString("icon");
+                        String main_deskription = main_weather_icon.getString("description");
+                        main_deskription = main_deskription.substring(0, 1).toUpperCase() + main_deskription.substring(1);
 
                         dateTime = (int) json.get("dt");
                         @SuppressLint("SimpleDateFormat") String result = new java.text.SimpleDateFormat("HH:mm").format(new Date(dateTime * 1000L));
 
 
-
-                        String description = object.getString("description");
-                        description = description.substring(0, 1).toUpperCase() + description.substring(1);
-
-                        String icon = object.getString("icon");
-                        double Temperature = temp1.getDouble("temp");
-                        String temp = String.valueOf(Temperature);
-
-                        String finalDescription = description;
+                        String finalMain_deskription = main_deskription;
                         runOnUiThread(new Runnable() {
 
                             @Override
                             public void run() {
-
-                                // Stuff that updates the UI
+                                tvDesk.setText(finalMain_deskription);
+                                setImage(ivIcon, main_icon);
                                 tvTemp.setText(temp+"℃");
-                                setImage(ivIcon, icon);
-                                tvDesk.setText(finalDescription);
-                                tvTime.setText(result);
+                                tvTime.setText("Актуально на: " + result);
+                                tvSunrise.setText(sunrise_text);
+                                tvSunset.setText(sunset_text);
                             }
                         });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                ;
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    private void api_key() {
+        Logger.getLogger(OkHttpClient.class.getName()).setLevel(Level.FINE);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+//                .url("https://api.openweathermap.org/data/2.5/weather?q=yakutsk&appid=23df53519973b7a0f5b39b79e5b9aec4&units=metric&lang=ru")
+                .url("https://api.openweathermap.org/data/2.5/forecast?q=Yakutsk&appid=23df53519973b7a0f5b39b79e5b9aec4&units=metric&lang=ru")
+                .get()
+                .build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Response response = client.newCall(request).execute();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                }
+
+                @SuppressLint("SimpleDateFormat")
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    String responseData = response.body().string();
+                    Logger.getLogger(OkHttpClient.class.getName()).setLevel(Level.FINE);
+                    try {
+                        JSONObject json = new JSONObject(responseData);
+//                        JSONArray jArray = json.getJSONArray("list");
+//                        System.out.println("****JARRAY****" + jArray.length());
+//
+//                        JSONObject main_data = jArray.getJSONObject(0);
+//
+//                        JSONObject main = main_data.getJSONObject("main");
+//                        String temp = main.getString("temp");
+//
+//
+//                        JSONArray main_weather = main_data.getJSONArray("weather");
+//                        JSONObject main_weather_icon = main_weather.getJSONObject(0);
+//                        String main_icon = main_weather_icon.getString("icon");
+//                        String main_deskription = main_weather_icon.getString("description");
+//                        main_deskription = main_deskription.substring(0, 1).toUpperCase() + main_deskription.substring(1);
+//
+//                        dateTime = (int) main_data.get("dt");
+//                        @SuppressLint("SimpleDateFormat") String result = new java.text.SimpleDateFormat("HH:mm").format(new Date(dateTime * 1000L));
+//
+//
+//                        String finalMain_deskription = main_deskription;
+//                        runOnUiThread(new Runnable() {
+//
+//                            @Override
+//                            public void run() {
+//                                tvDesk.setText(finalMain_deskription);
+//                                setImage(ivIcon, main_icon);
+//                                tvTemp.setText(temp+"℃");
+//                                tvTime.setText("Актуально на: " + result);
+//                            }
+//                        });
+
+
+
+
+
+
+
+
+//                        for (int a=1; a<6; a++)
+//                        {
+//                            JSONObject forecast_data = jArray.getJSONObject(a);
+//                            JSONArray weather = forecast_data.getJSONArray("weather");
+//                            JSONObject weather_object = weather.getJSONObject(0);
+//                            String icon = weather_object.getString("icon");
+//                        }
+
+//                        for(int i=0; i<jArray.length(); i++){
+//                            JSONObject json_data = jArray.getJSONObject(i);
+//
+//                            Log.d("log_tag", "test = " + i + " " + json_data.getString("dt_txt")
+//                            );
+//                        }
+
+//                        JSONObject temp1 = json.getJSONObject("main");
+//
+//
+//                        JSONArray weather = json.getJSONArray("weather");
+//                        JSONObject object = weather.getJSONObject(0);
+//
+//                        dateTime = (int) json.get("dt");
+//                        @SuppressLint("SimpleDateFormat") String result = new java.text.SimpleDateFormat("HH:mm").format(new Date(dateTime * 1000L));
+//
+//
+//
+//                        String description = object.getString("description");
+//                        description = description.substring(0, 1).toUpperCase() + description.substring(1);
+//
+//                        String icon = object.getString("icon");
+//                        double Temperature = temp1.getDouble("temp");
+//                        String temp = String.valueOf(Temperature);
+//
+//                        String finalDescription = description;
+
 
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -195,6 +309,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
